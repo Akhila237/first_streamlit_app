fc84863442e34d037dd37f790eaa0d909b21a0f1
RAGIPINDI GANGA AKHILA <124127276+Akhila237@users.noreply.github.com>
2023-09-12 16:56:00 +0530

    Update streamlit_app.py
    Capitalized Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry oatmeal')
-streamlit.text('kale, spinach & rocket smoothie')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
